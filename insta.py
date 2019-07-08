342d43ec89aca86f9898b9ba1a49f484cb55abea
labnet kopp <suporte@labnet.nce.ufrj.br>
2019-07-07 22:25:44 -0300

    insta

--- insta.py
+++ insta.py
@@ -5,10 +5,10 @@ from time import sleep
 
 class Instagram:
     def __init__(self):
-        self.username,self.password = models.get_credentials('instagram')
         self.bot = webdriver.Firefox()
     def login(self):
         bot = self.bot
+        self.username,self.password = models.get_credentials('instagram')
         bot.get('https://www.instagram.com/accounts/login/')
         sleep(3)
         username = bot.find_element_by_name('username')
@@ -16,6 +16,62 @@ class Instagram:
         username.send_keys(self.username)
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
+        sleep(3)
+        try:
+            bot.find_element_by_xpath('/html/body/div[3]/div/div/div[3]/button[2]').click()
+            sleep(0.3)
+        except:
+            pass
+
+
+    def curtir(self,pages=10):
+        bot = self.bot
+        bot.get('https://www.instagram.com')
+        curtida = 0
+        while curtida < pages:
+            print(curtida)
+            sleep(4)
+            try:
+                bot.find_element_by_xpath('//article//section/span[1]/button/span[@aria-label="Curtir"]').click()
+                curtida +=1
+            except Exception as e:
+                print(e)
+                bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+
+
+
+    def curtir_hashtag(self, hashtag, n=5):
+        bot = self.bot
+        links = models.get_shortcode(hashtag,n)
+        for post in links:
+            sleep(3)
+            try:
+                bot.get(post)
+                heart = bot.find_element_by_xpath('//article//section/span[1]/button/span[@aria-label="Curtir"]')
+                sleep(2)
+                heart.click()
+                print('o',end='')
+            except:
+                print('.',end='')
 
 insta = Instagram()
 insta.login()
+#insta.curtir(10)
+insta.curtir_hashtag('bitcoin',1)
+insta.curtir_hashtag('tkditf',1)
+insta.curtir_hashtag('internetofthings',1)
+
+'''    def curtir(self,pages=0):
+        bot = self.bot
+        bot.get('https://www.instagram.com/')
+        for i in range(0,pages):
+            print(i)
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            sleep(3)
+        for i in range(10):
+            posts = bot.find_elements_by_class_name('fr66n')
+            for post in posts:
+                to_click = post.find_element_by_class_name('glyphsSpriteHeart__outline__24__grey_9')
+                to_click.click()
+                sleep(4)
+'''
--- models.py
+++ models.py
@@ -1,4 +1,27 @@
 def get_credentials(socialmedia):
     with open('credentials.txt', 'r') as file:
         credentials = eval(str(file.read()))
-    return credentials[socialmedia]['user'], credentials[socialmedia]['password'] 
+    return credentials[socialmedia]['user'], credentials[socialmedia]['password']
+
+def get_shortcode(hashtag='internetofthings',num=5):
+    import requests
+    import json
+
+    lista = []
+    content = requests.get('https://www.instagram.com/explore/tags/'+str(hashtag)+'/?__a=1').text
+    a = json.loads(str(content))
+    for i in range(num):
+        try:
+            for b in a['graphql']['hashtag']['edge_hashtag_to_media']['edges']:
+                code = b['node']['shortcode']
+                if code not in lista:
+                    link = 'https://www.instagram.com/p/'+code
+                    lista.append(str(link))
+            #print(lista)
+            end_cursor = a['graphql']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor']
+            content = requests.get('https://www.instagram.com/explore/tags/'+str(hashtag)+'/?__a=1&max_id='+str(end_cursor)).text
+            a = json.loads(str(content))
+        except Exception as e:
+            print(e)
+            return lista
+    return lista
